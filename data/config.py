33caff93e3f1d65e0789fec3d39a3786fb25cf7d
MakerScript <104077050+MakerScript@users.noreply.github.com>
2023-05-19 15:27:12 +0700

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,7 +1,2 @@
-admins = 0 and 0 
-
-token = ('5569348960:AAEQOOlBNho48styyXp9bru-8h24jZ6rGqw')
-
-# key = ('PlutoWare:*hD%9BamDurE$Tt*H@PVakD')
-
-# webkey = ('')
+token = '5569348960:AAEQOOlBNho48styyXp9bru-8h24jZ6rGqw'
+admins = 0 and 0
